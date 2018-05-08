103fd3767baa98f7d25c39ad8805dc9da7b2769a
Andrei Chernyshev <andrei.chernyshev@outfittery.de>
2018-05-08 11:57:28 +0200

    update to support kubeless from 0.4.0

--- main.go
+++ main.go
@@ -11,10 +11,19 @@ import (
 	"strconv"
 	"github.com/robfig/cron"
 	"time"
-	"strings"
 	"net/http"
 	"os"
 )
+
+type CronJobTriggerResource struct {
+	Metadata *metav1.ObjectMeta `json:"metadata"`
+	Spec CronSpec `json:"spec"`
+}
+
+type CronSpec struct {
+	FunctionName string `json:"function-name"`
+    Schedule string `json:"schedule"`
+}
 
 type FunctionResource struct {
 	Metadata *metav1.ObjectMeta `json:"metadata"`
@@ -22,7 +31,6 @@ type FunctionResource struct {
 }
 
 type Spec struct {
-	Schedule string  `json:"schedule,omitempty"`
 	Type     string  `json:"type"`
 	Service  Service `json:"service"`
 }
@@ -39,13 +47,26 @@ func (m *FunctionResource) GetMetadata() *metav1.ObjectMeta {
 	return m.Metadata
 }
 
+func (m *CronJobTriggerResource) GetMetadata() *metav1.ObjectMeta {
+	return m.Metadata
+}
+
 type FunctionResourceList struct {
 	Metadata *metav1.ListMeta   `json:"metadata"`
 	Items    []FunctionResource `json:"items"`
 }
 
+type CronJobTriggerResourceList struct {
+	Metadata *metav1.ListMeta   `json:"metadata"`
+	Items    []CronJobTriggerResource `json:"items"`
+}
+
 // Require for MyResourceList to implement k8s.ResourceList
 func (m *FunctionResourceList) GetMetadata() *metav1.ListMeta {
+	return m.Metadata
+}
+
+func (m *CronJobTriggerResourceList) GetMetadata() *metav1.ListMeta {
 	return m.Metadata
 }
 
@@ -71,7 +92,7 @@ type CronJob struct {
 func main() {
 	var crons = map[string]*CronJob{}
 	log.Print("Start")
-	//kubeConfigPath := "/home/user/.kube/config"
+	// kubeConfigPath := "/home/user/.kube/config"
 	kubeConfigPath := os.Getenv("KUBE_CONFIG") //"/home/user/.kube/config"
 	var client *k8s.Client
 	var err error
@@ -83,33 +104,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	k8s.Register("kubeless.io", "v1", "function", true, &FunctionResource{})
-	k8s.RegisterList("kubeless.io", "v1", "functions", true, &FunctionResourceList{})
-	var functions FunctionResourceList
+	k8s.Register("kubeless.io", "v1beta1", "functions", true, &FunctionResource{})
+	k8s.Register("kubeless.io", "v1beta1", "cronjobtriggers", true, &CronJobTriggerResource{})
+	k8s.RegisterList("kubeless.io", "v1beta1", "functions", true, &FunctionResourceList{})
+	k8s.RegisterList("kubeless.io", "v1beta1", "cronjobtriggers", true, &CronJobTriggerResourceList{})
+	
+	var cronTriggers CronJobTriggerResourceList
 	for {
-		err = client.List(context.Background(), k8s.AllNamespaces, &functions)
-		for f := range functions.Items {
-			function := functions.Items[f]
-			if function.Spec.Type == "Scheduled" {
+		err = client.List(context.Background(), k8s.AllNamespaces, &cronTriggers)
+		for f := range cronTriggers.Items {
+			cronTrigger := cronTriggers.Items[f]
+			var function FunctionResource
+			err = client.Get(context.Background(), *cronTrigger.Metadata.Namespace, cronTrigger.Spec.FunctionName, &function)
+			if err == nil {
 				url := "http://" + *function.Metadata.Name + "." + *function.Metadata.Namespace + ":" + strconv.Itoa(function.Spec.Service.Ports[0].Port)
-				if strings.HasPrefix(function.Spec.Schedule, "cron") {
-					pattern := function.Spec.Schedule[5:len(function.Spec.Schedule)-1]
-					if crons[*function.Metadata.Name] != nil && crons[*function.Metadata.Name].Pattern != pattern {
-						crons[*function.Metadata.Name].Cron.Stop()
-						delete(crons, *function.Metadata.Name)
-					}
-					if crons[*function.Metadata.Name] == nil || crons[*function.Metadata.Name].Pattern != pattern {
-						log.Printf("Function '%s' with schedule '%s' was descovered", *function.Metadata.Name, pattern)
-						c := cron.New()
-						c.AddFunc(pattern, func() {
-							log.Printf("Trigger '%s' function - GET - %s", *function.Metadata.Name, url)
-							http.Get(url)
-						})
-						crons[*function.Metadata.Name] = &CronJob{pattern, c}
-						c.Start()
-					}
+				
+				pattern := cronTrigger.Spec.Schedule
+				if crons[*function.Metadata.Name] != nil && crons[*function.Metadata.Name].Pattern != pattern {
+					crons[*function.Metadata.Name].Cron.Stop()
+					delete(crons, *function.Metadata.Name)
 				}
+				if crons[*function.Metadata.Name] == nil || crons[*function.Metadata.Name].Pattern != pattern {
+					log.Printf("Function '%s' with schedule '%s' was descovered", *function.Metadata.Name, pattern)
+					c := cron.New()
+					c.AddFunc(pattern, func() {
+						log.Printf("Trigger '%s' function - GET - %s", *function.Metadata.Name, url)
+						http.Get(url)
+					})
+					crons[*function.Metadata.Name] = &CronJob{pattern, c}
+					c.Start()
+				}
+			} else {
+				log.Print(err);
 			}
+			
 		}
 		time.Sleep(5 * time.Second)
 	}
